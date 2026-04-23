e8a13550ae54fd24c56c0f85e2b540e15c5a49af
simon <simon.pradka@gmail.com>
2026-04-23 10:55:01 +0200

    refactor: :lipstick: Improved UI overall, simplified style and switched icons
    Icons are now bold and full.

--- frontend/src/components/EnableMenu.tsx
+++ frontend/src/components/EnableMenu.tsx
@@ -4,44 +4,44 @@ interface MenuProps {
 }
 
 const EnableMenu = ({selected, onSelect}: MenuProps) => {
+    const menuParams = 'menu-active rounded'
+    const menuIconParams = 'size-6 dark:fill-base-content fill-white'
     const selectedLogic = (buttonName: string) => {
         if (selected !== buttonName) {
             onSelect(buttonName)
         }
-        // This means when the button is clicked for the second time
-        // Not used since its not really clever and best practice.
-        // if (selected === buttonName) {
-        //     onSelect(null)
-        // }
     }
 
     return(
-        <ul className="menu bg-base-200 rounded-box">
-            <li className={selected === 'Home' ? 'menu-active rounded-2xl': ''} onClick={() => selectedLogic('Home')}>
+        <ul className="menu rounded-box">
+            <li className={selected === 'Home' ? menuParams : ''} onClick={() => selectedLogic('Home')}>
                 <a className="tooltip tooltip-right" data-tip="Home">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={selected === 'Home' ? menuIconParams : 'size-6'}>
+                <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
                 </svg>
                 </a>
+
             </li>
-            <li className={selected === 'Add' ? 'menu-active rounded-2xl': ''} onClick={() => {selectedLogic('Add')}}>
+            <li className={selected === 'Add' ? menuParams : ''} onClick={() => {selectedLogic('Add')}}>
                 <a className="tooltip tooltip-right" data-tip="Add">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={selected === 'Add' ? menuIconParams : 'size-6'}>
+                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
                 </svg>
                 </a>
             </li>
-            <li className={selected === 'Incidents' ? 'menu-active rounded-2xl': ''} onClick={() => {selectedLogic('Incidents')}}>
+            <li className={selected === 'Incidents' ? menuParams : ''} onClick={() => {selectedLogic('Incidents')}}>
                 <a className="tooltip tooltip-right" data-tip="Incidents">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={selected === 'Incidents' ? menuIconParams : 'size-6'}>
+                <path d="M5.625 3.75a2.625 2.625 0 1 0 0 5.25h12.75a2.625 2.625 0 0 0 0-5.25H5.625ZM3.75 11.25a.75.75 0 0 0 0 1.5h16.5a.75.75 0 0 0 0-1.5H3.75ZM3 15.75a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75ZM3.75 18.75a.75.75 0 0 0 0 1.5h16.5a.75.75 0 0 0 0-1.5H3.75Z" />
                 </svg>
                 </a>
             </li>
-            <li className={selected === 'Report' ? 'menu-active rounded-2xl': ''} onClick={() => {selectedLogic('Report')}}>
+            <li className={selected === 'Report' ? menuParams : ''} onClick={() => {selectedLogic('Report')}}>
                 <a className="tooltip tooltip-right" data-tip="Report">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={selected === 'Report' ? menuIconParams : 'size-6'}>
+                <path fillRule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625ZM7.5 15a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 7.5 15Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H8.25Z" clipRule="evenodd" />
+                <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
                 </svg>
                 </a>
             </li>
